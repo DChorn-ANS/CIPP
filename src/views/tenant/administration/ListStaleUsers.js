d20aff1cbf686f01d2d9dbdc45f74371a22c9a5a
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-03 11:25:42 -0700

    Create ListStaleUsers.js

---
+++ src/views/tenant/administration/ListStaleUsers.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { CippPageList } from 'src/components/layout'
+import { CellTip } from 'src/components/tables'
+
+const columns = [
+  {
+    name: 'User',
+    selector: (row) => row['DisplayName'],
+    sortable: true,
+    cell: (row) => CellTip(row['DisplayName']),
+    exportSelector: 'User',
+  },
+  {
+    name: 'UPN',
+    selector: (row) => row['UPN'],
+    sortable: true,
+    cell: (row) => CellTip(row['UPN']),
+    exportSelector: 'UPN',
+  },
+  {
+    name: 'lastSignInDate',
+    selector: (row) => row['lastSignInDate'],
+    sortable: true,
+    exportSelector: 'lastSignInDate',
+  },
+]
+
+const StaleUsersList = () => {
+  const tenant = useSelector((state) => state.app.currentTenant)
+
+  return (
+    <CippPageList
+      capabilities={{ allTenants: true, helpContext: 'https://google.com' }}
+      title="Stale Users Report"
+      tenantSelector={false}
+      showAllTenantSelector={false}
+      datatable={{
+        reportName: `${tenant?.defaultDomainName}-StaleUsers`,
+        path: '/api/ListStaleUsers',
+        params: { TenantFilter: tenant?.defaultDomainName },
+        columns,
+      }}
+    />
+  )
+}
+
+export default StaleUsersList
