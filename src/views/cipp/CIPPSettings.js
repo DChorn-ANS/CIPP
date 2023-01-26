5fdb89e3c50f79385d1a314870f9778937c5bee3
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-26 12:39:53 -0700

    Update CIPPSettings.js

--- src/views/cipp/CIPPSettings.js
+++ src/views/cipp/CIPPSettings.js
@@ -28,7 +28,6 @@ import {
   useLazyGenericGetRequestQuery,
   useLazyGenericPostRequestQuery,
   useLazyListNotificationConfigQuery,
-  useLazyExecDebugMode,
 } from 'src/store/api/app'
 import {
   useExecAddExcludeTenantMutation,
@@ -90,7 +89,7 @@ const CIPPSettings = () => {
           Maintenance
         </CNavItem>
         <CNavItem active={active === 7} onClick={() => setActive(7)} href="#">
-          Troubleshooting
+          Maintenance2
         </CNavItem>
       </CNav>
       <CTabContent>
@@ -112,8 +111,8 @@ const CIPPSettings = () => {
         <CTabPane visible={active === 6} className="mt-3">
           <Maintenance />
         </CTabPane>
-        <CTabPane visible={active === 7} className="mt-3">
-          <Troubleshooting />
+        <CTabPane visible={active === 6} className="mt-3">
+          <Maintenance2 />
         </CTabPane>
       </CTabContent>
     </CippPage>
@@ -1262,60 +1261,138 @@ const Maintenance = () => {
   )
 }
 
-const Troubleshooting = () => {
-  const [listDebugMode, listDebugModeResult] = useLazyGenericGetRequestQuery()
-  const [setDebugMode, setDebugModeResult] = useLazyExecDebugMode()
-  //const [rebootFunctionApp, rebootFunctionAppResult] = useLazyRebootFunctionAppQuery()
+const Maintenance2 = () => {
+  const [selectedScript, setSelectedScript] = useState()
+  const [listBackend, listBackendResult] = useLazyGenericGetRequestQuery()
+  const [listScript, listScriptResult] = useLazyGenericGetRequestQuery()
+  const [listScriptLink, listScriptLinkResult] = useLazyGenericGetRequestQuery()
 
-  const onSubmit = (values) => {
-    console.log(values)
-    setDebugMode(values)
+  const handleSubmit = async (values) => {
+    listScript({ path: 'api/ExecMaintenanceScripts', params: values })
+    setSelectedScript(values.ScriptFile)
   }
 
+  const handleGetLink = () => {
+    console.log('Making link')
+    listScriptLink({
+      path: 'api/ExecMaintenanceScripts',
+      params: { ScriptFile: selectedScript, MakeLink: 'True' },
+    })
+  }
   return (
     <>
+      {listBackendResult.isUninitialized && listBackend({ path: 'api/ExecMaintenanceScripts' })}
       <CRow>
         <CCol>
           <CCard className="options-card">
             <CCardHeader>
-              <CCardTitle className="d-flex justify-content-between">Debug Mode</CCardTitle>
+              <CCardTitle className="d-flex justify-content-between">Maintenance</CCardTitle>
             </CCardHeader>
             <CCardBody>
               <Form
                 initialValues={{}}
-                onSubmit={onSubmit}
-                render={({ onSubmit, submitting, values }) => {
+                onSubmit={handleSubmit}
+                render={({ handleSubmit, submitting, values }) => {
                   return (
-                    <CForm onSubmit={onSubmit}>
-                      {setDebugModeResult.isFetching && (
-                        <CCallout color="info">
-                          <CSpinner>Loading</CSpinner>
-                        </CCallout>
+                    <CForm onSubmit={handleSubmit}>
+                      {listBackendResult.isFetching && (
+                        <>
+                          <CRow>
+                            <CCol>
+                              <Skeleton count={5} />
+                            </CCol>
+                          </CRow>
+                        </>
                       )}
-                      {setDebugModeResult.isSuccess && (
-                        <CCallout color="info">{setDebugModeResult.data?.Results}</CCallout>
+                      {!listBackendResult.isFetching && listBackendResult.isSuccess && (
+                        <>
+                          <CRow>
+                            <CCol>
+                              <RFFCFormSelect
+                                name="ScriptFile"
+                                label="Script File"
+                                placeholder="-- Select a script --"
+                                values={listBackendResult.data.ScriptFiles}
+                              />
+                            </CCol>
+                          </CRow>
+                          <CRow className="mb-3">
+                            <CCol>
+                              <CButton type="submit" disabled={submitting}>
+                                <FontAwesomeIcon icon={faScroll} className="me-2" />
+                                Load Script
+                              </CButton>
+                            </CCol>
+                          </CRow>
+                        </>
                       )}
-                      {setDebugModeResult.isError && (
-                        <CCallout color="danger">
-                          Could not connect to API: {setDebugModeResult.error.message}
-                        </CCallout>
-                      )}
-                      <CCol>
-                        <CCol>
-                          <RFFCFormSwitch
-                            name="setDebugMode"
-                            label="Enable Debug Logging Mode"
-                            value={false}
-                          />
-                        </CCol>
-                        <CButton type="submit">Save Debug Setting</CButton>
-                      </CCol>
                     </CForm>
                   )
                 }}
               />
             </CCardBody>
           </CCard>
+        </CCol>
+      </CRow>
+      <CRow>
+        <CCol>
+          {listScriptResult.isFetching && (
+            <CCard>
+              <CCardBody>
+                <Skeleton count={10} />
+              </CCardBody>
+            </CCard>
+          )}
+          {!listScriptResult.isFetching && listScriptResult.isSuccess && (
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Script Details</CCardTitle>
+              </CCardHeader>
+              <CCardBody>
+                <p>
+                  <CButton type="submit" onClick={handleGetLink}>
+                    <FontAwesomeIcon icon={faLink} className="me-2" />
+                    Create Link
+                  </CButton>
+                </p>
+                {listScriptLinkResult.isSuccess && (
+                  <p>
+                    {listScriptLinkResult.data.Link !== undefined && (
+                      <>
+                        <p>
+                          Copy this text into a PowerShell terminal, we recommend Azure Cloud Shell.
+                          Azure modules and the az command line utilties are required for these
+                          scripts to work. The link is valid for 5 minutes.
+                        </p>
+                        <CippCodeBlock
+                          language="text"
+                          showLineNumbers={false}
+                          wrapLongLines={true}
+                          code={
+                            'irm ' +
+                            window.location.origin +
+                            listScriptLinkResult.data.Link +
+                            ' | iex'
+                          }
+                        />
+                      </>
+                    )}
+                  </p>
+                )}
+                {listScriptResult.data.ScriptContent !== undefined && (
+                  <p>
+                    <h5>Maintenance Script Contents</h5>
+                    <CippCodeBlock
+                      language="powershell"
+                      showLineNumbers={true}
+                      wrapLongLines={false}
+                      code={Buffer.from(listScriptResult.data.ScriptContent, 'base64').toString()}
+                    />
+                  </p>
+                )}
+              </CCardBody>
+            </CCard>
+          )}
         </CCol>
       </CRow>
     </>
