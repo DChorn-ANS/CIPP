ddcacca1623f12fee60571a55b7d6df084d1b51f
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-01-27 11:26:25 -0700

    Update CIPPSettings.js

--- src/views/cipp/CIPPSettings.js
+++ src/views/cipp/CIPPSettings.js
@@ -1263,7 +1263,7 @@ const Maintenance = () => {
   )
 }
 
-const Troubleshooting = () => {
+const DebugMode = () => {
   const [listDebugMode, listDebugModeResult] = useLazyGenericGetRequestQuery()
   const [setDebugMode, setDebugModeResult] = useLazyGenericPostRequestQuery()
   const onSubmit = (values) => {
@@ -1323,5 +1323,124 @@ const Troubleshooting = () => {
         </CCard>
       )}
     </>
+  )
+}
+
+const Troubleshooting = () => {
+  const [listBackend, listBackendResult] = useLazyGenericGetRequestQuery()
+
+  return (
+    <div>
+      {listBackendResult.isUninitialized && listBackend({ path: 'api/ExecBackendURLs' })}
+      <>
+        <CRow className="mb-3">
+          <CCol md={4}>
+            <CCard>
+              <DebugMode />
+            </CCard>
+          </CCol>
+          <CCol md={4}>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Key Vault</CCardTitle>
+              </CCardHeader>
+              <CCardBody className="equalheight">
+                <CRow className="mb-3">
+                  The keyvault allows you to check token information. By default you do not have
+                  access.
+                </CRow>
+                <a
+                  target={'_blank'}
+                  href={listBackendResult.data?.Results?.KeyVault}
+                  rel="noreferrer"
+                >
+                  <CButton>Go to Keyvault</CButton>
+                </a>
+              </CCardBody>
+            </CCard>
+          </CCol>
+          <CCol md={4}>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Static Web App (Role Management)</CCardTitle>
+              </CCardHeader>
+              <CCardBody className="equalheight">
+                <CRow className="mb-3">
+                  The Static Web App role management allows you to invite other users to the
+                  application.
+                </CRow>
+                <a
+                  target={'_blank'}
+                  href={listBackendResult.data?.Results?.SWARoles}
+                  rel="noreferrer"
+                >
+                  <CButton>Go to Role Management</CButton>
+                </a>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+        <CRow className="mb-3">
+          <CCol md={4}>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Function App (Deployment Center)</CCardTitle>
+              </CCardHeader>
+              <CCardBody className="equalheight">
+                <CRow className="mb-3">
+                  The Function App Deployment Center allows you to run updates on the API
+                </CRow>
+                <a
+                  target={'_blank'}
+                  href={listBackendResult.data?.Results?.FunctionDeployment}
+                  rel="noreferrer"
+                >
+                  <CButton>Go to Function App Deployment Center</CButton>
+                </a>
+              </CCardBody>
+            </CCard>
+          </CCol>
+          <CCol md={4}>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Function App (Configuration)</CCardTitle>
+              </CCardHeader>
+              <CCardBody className="equalheight">
+                <CRow className="mb-3">
+                  At the Function App Configuration you can check the status of the API access to
+                  your keyvault
+                </CRow>
+                <a
+                  target={'_blank'}
+                  href={listBackendResult.data?.Results?.FunctionConfig}
+                  rel="noreferrer"
+                >
+                  <CButton>Go to Function App Configuration</CButton>
+                </a>
+              </CCardBody>
+            </CCard>
+          </CCol>
+          <CCol md={4}>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Function App (Overview)</CCardTitle>
+              </CCardHeader>
+              <CCardBody className="equalheight">
+                <CRow className="mb-3">
+                  At the function App Overview, you can stop and start the backend API
+                </CRow>
+                <a
+                  target={'_blank'}
+                  href={listBackendResult.data?.Results?.FunctionApp}
+                  rel="noreferrer"
+                >
+                  <CButton>Go to Function App Overview</CButton>
+                </a>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </>
+    </div>
   )
 }
