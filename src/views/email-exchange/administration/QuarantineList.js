8056487811ee4e9aa388d6480d3f2e61e9563da0
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-07-19 14:21:09 -0600

    Update QuarantineList.js

--- src/views/email-exchange/administration/QuarantineList.js
+++ src/views/email-exchange/administration/QuarantineList.js
@@ -52,7 +52,7 @@ const QuarantineList = () => {
             },
             {
               label: 'Deny',
-              color: 'danger',
+              color: 'warning',
               modal: true,
               modalUrl: `/api/ExecQuarantineManagement?TenantFilter=${tenant.defaultDomainName}&ID=${row.Identity}&Type=Deny`,
               modalMessage: 'Are you sure you want to deny this message?',
