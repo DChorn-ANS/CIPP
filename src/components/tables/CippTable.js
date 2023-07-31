277bd77622b6512cd015da3fc196b3c2b424ef9f
DChorn-ANS <94991453+DChorn-ANS@users.noreply.github.com>
2023-07-31 11:15:24 -0600

    Update CippTable.js

--- src/components/tables/CippTable.js
+++ src/components/tables/CippTable.js
@@ -108,11 +108,11 @@ const customSort = (rows, selector, direction) => {
     let comparison = 0
 
     if (
-      aField?.toString().localeCompare(bField, 'en', { numeric: true, sensitivity: 'base' }) > 0
+      aField.toString().localeCompare(bField.toString(), 'en', { numeric: true, sensitivity: 'base' }) > 0
     ) {
       comparison = 1
     } else if (
-      aField?.toString().localeCompare(bField, 'en', { numeric: true, sensitivity: 'base' }) < 0
+      aField.toString().localeCompare(bField.toString(), 'en', { numeric: true, sensitivity: 'base' }) < 0
     ) {
       comparison = -1
     }
